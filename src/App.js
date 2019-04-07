fc83f6cbb0aedc770bde7f5e44bae815f431532e
Erin Knight <erin.knight@headspring.com>
2019-04-06 22:27:47 -0500

    Update node-sass to latest

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Round from './Round.js';
-import config from '../config.js';
+import { config } from '../config.js';
 require('../assets/css/style.scss');
 
 injectTapEventPlugin();
