c5ac719861bcd2c84974dd21374b8dc6543d2baa
Erin Knight <erin.knight@headspring.com>
2019-04-08 21:40:31 -0500

    Update to match paper bracket

--- src/Round0.js
+++ src/Round0.js
@@ -10,13 +10,13 @@ const Round = ({ data, games }) => {
     <div className="round">
       <h3>Play-In Round</h3>
       <BracketGame pet1={pet(31)} pet2={pet(32)} game={games[1]} />
-      <BracketGame pet1={pet(28)} pet2={pet(35)} game={games[2]} />
-      <BracketGame pet1={pet(30)} pet2={pet(33)} game={games[3]} />
-      <BracketGame pet1={pet(29)} pet2={pet(34)} game={games[4]} />
-      <BracketGame pet1={pet(27)} pet2={pet(36)} game={games[5]} />
-      <BracketGame pet1={pet(26)} pet2={pet(37)} game={games[6]} />
-      <BracketGame pet1={pet(22)} pet2={pet(38)} game={games[7]} />
-      <BracketGame pet1={pet(25)} pet2={pet(39)} game={games[8]} />
+      <BracketGame pet1={pet(24)} pet2={pet(39)} game={games[2]} />
+      <BracketGame pet1={pet(28)} pet2={pet(35)} game={games[3]} />
+      <BracketGame pet1={pet(27)} pet2={pet(36)} game={games[4]} />
+      <BracketGame pet1={pet(30)} pet2={pet(33)} game={games[5]} />
+      <BracketGame pet1={pet(22)} pet2={pet(38)} game={games[6]} />
+      <BracketGame pet1={pet(29)} pet2={pet(34)} game={games[7]} />
+      <BracketGame pet1={pet(26)} pet2={pet(37)} game={games[8]} />
     </div>
   ) : <div></div>;
 }
--- src/Round1.js
+++ src/Round1.js
@@ -16,21 +16,21 @@ const Round = ({ data, games }) => {
     <div className="round">
       <h3>Round of 32</h3>
       <BracketGame pet1={pet(0)} pet2={winnerFromGame(1)} game={games[9]} />
-      <BracketGame pet1={pet(16)} pet2={pet(15)} game={games[10]} />
+      <BracketGame pet1={pet(15)} pet2={pet(16)} game={games[10]} />
       <BracketGame pet1={pet(8)} pet2={pet(23)} game={games[11]} />
-      <BracketGame pet1={pet(24)} pet2={pet(7)} game={games[12]} />
-      <BracketGame pet1={pet(3)} pet2={winnerFromGame(2)} game={games[13]} />
-      <BracketGame pet1={pet(19)} pet2={pet(12)} game={games[14]} />
+      <BracketGame pet1={pet(7)} pet2={winnerFromGame(2)} game={games[12]} />
+      <BracketGame pet1={pet(3)} pet2={winnerFromGame(3)} game={games[13]} />
+      <BracketGame pet1={pet(12)} pet2={pet(19)} game={games[14]} />
       <BracketGame pet1={pet(11)} pet2={pet(20)} game={games[15]} />
-      <BracketGame pet1={winnerFromGame(3)} pet2={pet(4)} game={games[16]} />
-      <BracketGame pet1={pet(1)} pet2={winnerFromGame(4)} game={games[17]} />
-      <BracketGame pet1={pet(17)} pet2={pet(14)} game={games[18]} />
-      <BracketGame pet1={pet(9)} pet2={winnerFromGame(7)} game={games[19]} />
-      <BracketGame pet1={winnerFromGame(8)} pet2={pet(6)} game={games[20]} />
-      <BracketGame pet1={pet(2)} pet2={winnerFromGame(5)} game={games[21]} />
-      <BracketGame pet1={pet(18)} pet2={pet(13)} game={games[22]} />
+      <BracketGame pet1={winnerFromGame(4)} pet2={pet(4)} game={games[16]} />
+      <BracketGame pet1={pet(1)} pet2={winnerFromGame(5)} game={games[17]} />
+      <BracketGame pet1={pet(14)} pet2={pet(17)} game={games[18]} />
+      <BracketGame pet1={pet(9)} pet2={winnerFromGame(6)} game={games[19]} />
+      <BracketGame pet1={pet(6)} pet2={pet(25)} game={games[20]} />
+      <BracketGame pet1={pet(2)} pet2={winnerFromGame(7)} game={games[21]} />
+      <BracketGame pet1={pet(13)} pet2={pet(18)} game={games[22]} />
       <BracketGame pet1={pet(10)} pet2={pet(21)} game={games[23]} />
-      <BracketGame pet1={winnerFromGame(6)} pet2={pet(5)} game={games[24]} />
+      <BracketGame pet1={winnerFromGame(8)} pet2={pet(5)} game={games[24]} />
     </div>
   ) : <div></div>;
 }
